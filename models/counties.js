548cf8ed4dbcdcf2e849babf930c4b0f5946be05
Nicanor008 <nicanorkorir008@gmail.com>
2022-08-05 17:32:05 +0300

    More security on the project model

--- models/counties.js
+++ models/counties.js
@@ -1,35 +1,6 @@
 import pkg from "mongoose";
 const { Schema, model } = pkg;
 
-const countiesSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    code: {
-      type: Number,
-      required: true,
-    },
-    capital: {
-      type: String,
-      required: true,
-    },
-    population: {
-      2009: Number,
-      2019: Number,
-    },
-    formerProvince: {
-      type: String
-    },
-    areaInKM2: {
-      type: Number
-    },
-    subCounty: {
-      type: Array
-    }
-  },
-);
+const countiesSchema = new Schema({ any: Schema.Types.Mixed });
 
 export default model("Counties", countiesSchema);
